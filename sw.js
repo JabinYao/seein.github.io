555fe49bb3fb07ebbca91c08d4edf455792f1849
JabinYao <51532725+JabinYao@users.noreply.github.com>
2026-03-18 11:13:44 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = 'time-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// 安装Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存中有请求的资源，则返回缓存版本
+        if (response) {
+          return response;
+        }
+        
+        // 否则发起网络请求
+        return fetch(event.request).then(
+          response => {
+            // 检查是否成功获取资源
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 克隆响应，因为响应流只能读取一次
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
